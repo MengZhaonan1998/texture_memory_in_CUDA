f92983e7a1fe4f846dcb512902e1bf3fb1604a44
Zhaonan Meng <mengzn1998@gmail.com>
2023-10-17 23:11:14 +0800

    random number generator is used.

--- matrix_addition/matrix_addition.cpp
+++ matrix_addition/matrix_addition.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <cstdlib>
+#include <random>
 #include <time.h>
 
 void cpu_addition(double* h_A, double* h_B, double* h_C, int N)
@@ -14,36 +15,35 @@ void cpu_addition(double* h_A, double* h_B, double* h_C, int N)
     std::cout << "CPU computation completed! It took " << ((float)time) / CLOCKS_PER_SEC << " seconds" << std::endl;
 };
 
-
 // This program is designed to test the performance of matrix addition computed by different techniques 
 int main()
 {
-    int N = 4096;
-    double* h_A = new double[N * N];
-    double* h_B = new double[N * N];
-    double* h_C = new double[N * N];
+    int N = 3;// 4096; // Problem size
 
-    
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-        {
-            h_A[i * N + j] = (double)(i+j);
-            h_B[i * N + j] = (double)(i-j);
-            h_C[i * N + j] = (double)0.0;
-        }
-    /*
-    for (int i = 0; i < N; i++)
+    double* h_A = new double[(size_t)N * N]{0.0};
+    double* h_B = new double[(size_t)N * N]{0.0};
+    double* h_C = new double[(size_t)N * N]{0.0};
+
+    double lower_bound = 0;
+    double upper_bound = 100;
+    std::uniform_real_distribution<double> unif(lower_bound, upper_bound);
+    std::default_random_engine re;
+    for (int i = 0; i < N * N; i++)
+    {   
+        h_A[i] = unif(re);
+        h_B[i] = unif(re);
+    }
+
+/*    for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
             std::cout << h_A[i * N + j] << " ";
         }
         std::cout << "\n" << std::endl;
-    }
-    */
+    }*/
 
     cpu_addition(h_A, h_B, h_C, N);
-
 
     delete [] h_A;
     delete [] h_B;
